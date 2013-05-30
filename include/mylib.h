e0f9762136c337e6021ddf8367e10bae370ae98d
john0611 <zhou_0611@163.com>
2013-05-30 12:52:08 +0800

    添加mylib.h文件，调用库时只要使用该头文件即可

---
+++ include/mylib.h
@@ -0,0 +1,22 @@
+#ifndef __MYLIB_H_
+#define __MYLIB_H_
+#include "std.h"
+#include "Socket.h"
+#include "CSocket.h"
+#include "Thread.h"
+#include "Statistics.h"
+#include "Mint.h"
+#include "MutexLock.h"
+#include "MrswLock.h"
+#include "Nonreentrant.h"
+#include "Debug.h"
+#include "TonyLowDebug.h"
+#include "ThreadManager.h"
+#include "ThreadPool.h"
+#include "MemoryManager.h"
+#include "Buffer.h"
+#include "Log.h"
+#include "Utils.h"
+#include "BaseLib.h"
+#endif
+
